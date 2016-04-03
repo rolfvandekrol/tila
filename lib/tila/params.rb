150598b56e363f6150e5aa7d6cbee26dd8464f5d
Rolf van de Krol <info@rolfvandekrol.nl>
2016-04-03 14:54:09 +0200

    Refactor Params and add specs

--- lib/tila/params.rb
+++ lib/tila/params.rb
@@ -2,20 +2,32 @@ module Tila
   module Params
     extend ActiveSupport::Concern
     include Modelable
-    include Objects
+
+    included do
+      helper_method :resource_params_name, :resource_params,
+        :permitted_resource_params_list, :permitted_resource_params
+    end
 
     protected
 
-    def update_resource_params
-      object.attributes = permitted_resource_params
-    end
-
-    def permitted_resource_params
-      raise "The permitted_resource_params method should be overriden"
+    def resource_params_name
+      resource_name
     end
 
     def resource_params
-      params.require(object_name)
+      params.require(resource_params_name)
+    end
+
+    def permitted_resource_params_list
+      raise NotImplementedError, "The permitted_resource_params_list method should be overriden"
+    end
+
+    def permitted_resource_params
+      resource_params.permit(*permitted_resource_params_list)
+    end
+
+    def update_resource_params
+      resource.attributes = permitted_resource_params
     end
   end
 end
---
+++ spec/modules/params_spec.rb
@@ -0,0 +1,78 @@
+
+describe Tila::Messages, type: :controller do
+  controller(BunniesController) do
+    include Tila::Params
+
+    def test_action
+      @resource_params_name = resource_params_name
+      @resource_params = resource_params
+      render 'shared/empty'
+    end
+
+    def test_action_raise1
+      @permitted_resource_params_list = permitted_resource_params_list
+      render 'shared/empty'
+    end
+
+    def test_action_raise2
+      @permitted_resource_params = permitted_resource_params
+      render 'shared/empty'
+    end
+  end
+
+  it 'exposes the correct helpers' do
+    routes.draw {
+      get "test_action" => "bunnies#test_action"
+      get "test_action_raise1" => "bunnies#test_action_raise1"
+      get "test_action_raise2" => "bunnies#test_action_raise2"
+    }
+
+    get :test_action, {bunny: {name: 'Oliver'}}
+    expect(assigns(:resource_params_name)).to eq('bunny')
+    expect(assigns(:resource_params)).to be_a(Hash)
+    expect(assigns(:resource_params)[:name]).to eq('Oliver')
+
+    expect {
+      get :test_action_raise1, {bunny: {name: 'Oliver'}}
+    }.to raise_error(NotImplementedError)
+
+    expect {
+      get :test_action_raise2, {bunny: {name: 'Oliver'}}
+    }.to raise_error(NotImplementedError)
+  end
+end
+
+describe Tila::Messages, type: :controller do
+  controller(BunniesController) do
+    include Tila::Params
+
+    def test_action1
+      @permitted_resource_params_list = permitted_resource_params_list
+      render 'shared/empty'
+    end
+
+    def test_action2
+      @permitted_resource_params = permitted_resource_params
+      render 'shared/empty'
+    end
+
+    protected
+
+    def permitted_resource_params_list
+      [:name]
+    end
+  end
+
+  it 'exposes the correct helpers' do
+    routes.draw {
+      get "test_action1" => "bunnies#test_action1"
+      get "test_action2" => "bunnies#test_action2"
+    }
+
+    get :test_action1
+    expect(assigns(:permitted_resource_params_list)).to eq([:name])
+
+    get :test_action2, {bunny: {name: 'Oliver'}}
+    expect(assigns(:permitted_resource_params)[:name]).to eq('Oliver')
+  end
+end
