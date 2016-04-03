db6b59b365febcc1faeda71e3983eca85eb7dd88
Rolf van de Krol <info@rolfvandekrol.nl>
2016-04-03 13:11:35 +0200

    Move spec around

--- spec/modules/modelable_spec.rb
+++ spec/modules/modelable_spec.rb
@@ -1,5 +1,29 @@
 
 describe Tila::Modelable, type: :controller do
+  controller(BunniesController) do
+    include Tila::Modelable
+
+    def test_action
+      @model = model
+      @resource_name = resource_name
+      @resources_name = resources_name
+      @model_name = model_name
+      render 'shared/empty'
+    end
+  end
+
+  it 'exposes the correct helpers' do
+    routes.draw { get "test_action" => "bunnies#test_action" }
+    get :test_action
+
+    expect(assigns(:model)).to eq(Bunny)
+    expect(assigns(:resource_name)).to eq('bunny')
+    expect(assigns(:resources_name)).to eq('bunnies')
+    expect(assigns(:model_name)).to eq('bunny')
+  end
+end
+
+describe Tila::Modelable, type: :controller do
   controller(Admin::BunniesController) do
     include Tila::Modelable
 
