31552d7e356f212ed04cf3359b216205a7b95873
Rolf van de Krol <info@rolfvandekrol.nl>
2016-04-13 08:03:21 +0200

    Use nesting in modelable spec

--- spec/modules/modelable_spec.rb
+++ spec/modules/modelable_spec.rb
@@ -1,48 +1,102 @@
 
 describe Tila::Modelable, type: :controller do
-  controller(BunniesController) do
-    include Tila::Modelable
+  context 'included in root level controller', type: :controller do
+    controller(BunniesController) do
+      include Tila::Modelable
 
-    def test_action
-      @model = model
-      @resource_name = resource_name
-      @resources_name = resources_name
-      @model_name = model_name
-      render 'shared/empty'
+      def test_action
+        @model = model
+        @resource_name = resource_name
+        @resources_name = resources_name
+        @model_name = model_name
+        render 'shared/empty'
+      end
+    end
+
+    describe '#model' do
+      it 'returns the model derived from the controller name' do
+        routes.draw { get "test_action" => "bunnies#test_action" }
+        get :test_action
+
+        expect(assigns(:model)).to eq(Bunny)
+      end
+    end
+
+    describe '#resource_name' do
+      it 'returns the resource name derived from the controller name' do
+        routes.draw { get "test_action" => "bunnies#test_action" }
+        get :test_action
+
+        expect(assigns(:resource_name)).to eq('bunny')
+      end
+    end
+
+    describe '#resources_name' do
+      it 'returns the resources name derived from the controller name' do
+        routes.draw { get "test_action" => "bunnies#test_action" }
+        get :test_action
+
+        expect(assigns(:resources_name)).to eq('bunnies')
+      end
+    end
+
+    describe '#model_name' do
+      it 'returns the model name derived from the controller name' do
+        routes.draw { get "test_action" => "bunnies#test_action" }
+        get :test_action
+
+        expect(assigns(:model_name)).to eq('bunny')
+      end
     end
   end
 
-  it 'exposes the correct helpers' do
-    routes.draw { get "test_action" => "bunnies#test_action" }
-    get :test_action
+  context 'included in namespaced controller', type: :controller do
+    controller(Admin::BunniesController) do
+      include Tila::Modelable
 
-    expect(assigns(:model)).to eq(Bunny)
-    expect(assigns(:resource_name)).to eq('bunny')
-    expect(assigns(:resources_name)).to eq('bunnies')
-    expect(assigns(:model_name)).to eq('bunny')
-  end
-end
-
-describe Tila::Modelable, type: :controller do
-  controller(Admin::BunniesController) do
-    include Tila::Modelable
-
-    def test_action
-      @model = model
-      @resource_name = resource_name
-      @resources_name = resources_name
-      @model_name = model_name
-      render 'shared/empty'
+      def test_action
+        @model = model
+        @resource_name = resource_name
+        @resources_name = resources_name
+        @model_name = model_name
+        render 'shared/empty'
+      end
     end
-  end
 
-  it 'exposes model helper' do
-    routes.draw { get "test_action" => "admin/bunnies#test_action" }
-    get :test_action
+    describe '#model' do
+      it 'returns the namespaced model derived from the controller name' do
+        routes.draw { get "test_action" => "admin/bunnies#test_action" }
+        get :test_action
 
-    expect(assigns(:model)).to eq(Admin::Bunny)
-    expect(assigns(:resource_name)).to eq('bunny')
-    expect(assigns(:resources_name)).to eq('bunnies')
-    expect(assigns(:model_name)).to eq('admin_bunny')
+        expect(assigns(:model)).to eq(Admin::Bunny)
+      end
+    end
+
+    describe '#resource_name' do
+      it 'returns the resource name derived from the controller name, without the namespace' do
+        routes.draw { get "test_action" => "admin/bunnies#test_action" }
+        get :test_action
+
+        expect(assigns(:resource_name)).to eq('bunny')
+      end
+    end
+
+    describe '#resources_name' do
+      it 'returns the resources name derived from the controller name, without the namespace' do
+        routes.draw { get "test_action" => "admin/bunnies#test_action" }
+        get :test_action
+
+        expect(assigns(:resources_name)).to eq('bunnies')
+      end
+    end
+
+    describe '#model_name' do
+      it 'returns the model name derived from the controller name, including the namespace' do
+        routes.draw { get "test_action" => "admin/bunnies#test_action" }
+        get :test_action
+
+        expect(assigns(:model_name)).to eq('admin_bunny')
+      end
+    end
   end
 end
