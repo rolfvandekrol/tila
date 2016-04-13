95415a4fd3ce23ac28ce5065546bd1b76bff47da
Rolf van de Krol <info@rolfvandekrol.nl>
2016-04-13 07:28:11 +0200

    Use nesting in actionable spec

--- spec/modules/actionable_spec.rb
+++ spec/modules/actionable_spec.rb
@@ -1,18 +1,20 @@
 
-describe Tila::Actionable, type: :controller do
-  controller(ApplicationController) do
-    include Tila::Actionable
+describe Tila::Actionable do
+  describe '#action', type: :controller do
+    controller(ApplicationController) do
+      include Tila::Actionable
 
-    def test_action
-      @action = action
-      render 'shared/empty'
+      def test_action
+        @action = action
+        render 'shared/empty'
+      end
     end
-  end
 
-  it 'exposes action helper' do
-    routes.draw { get "test_action" => "anonymous#test_action" }
-    get :test_action
+    it 'returns the current action' do
+      routes.draw { get "test_action" => "anonymous#test_action" }
+      get :test_action
 
-    expect(assigns(:action)).to eq(:test_action)
+      expect(assigns(:action)).to eq(:test_action)
+    end
   end
 end
